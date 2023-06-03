d1f16bd602124b7c93f6afc4551b4dce09e63efe
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2023-06-03 02:04:12 -0300

    feat: added function subs

--- src/twitch/twitch.service.ts
+++ src/twitch/twitch.service.ts
@@ -8,20 +8,96 @@ export class TwitchService {
   constructor(private httpService: HttpService)
   { }
 
-  async getSubs(userId: string, accessToken: string): Promise<AxiosResponse> {
+  async getSubs(userId: string, accessToken: string): Promise<any> {
     const headers = {
       Authorization: `Bearer ${accessToken}`,
       'Client-ID': APP_CONFIG.TWITCH.clientID, // Substitua pelo ID da sua aplicação Twitch
     };
-    const response = await this.httpService
+    let subs: any[] = [];
+    let cursor = '';
+    do {
+    const { data: subsData } : any = await this.httpService
       .get(`https://api.twitch.tv/helix/subscriptions?first=100&broadcaster_id=${userId}`, {
         headers,
+        params: {
+          broadcaster_id: userId,
+          first: 100,
+          after: cursor,
+        },
       })
-      .toPromise();
-    if (!response) {
-      throw new Error('Não foi possível obter a resposta da API do Twitch.');
-    }
-    return response;
+      .toPromise()
+      .catch((error) => {
+        Logger.error(error, 'Join');
+      });
+
+      if (!subsData) {
+        throw new Error('Não foi possível obter a resposta da API do Twitch.');
+      }
+
+      if (!Array.isArray(subsData.data)) {
+        throw new Error('A resposta da API do Twitch não está no formato esperado.');
+      }
+
+      subs.push(...subsData.data); // save views
+      cursor = subsData.pagination.cursor; // save last page
+
+      } while (cursor);
+
+      console.log("DATA", subs);
+      // Get Views ID
+      const userIds: any = subs.map((follow) => follow.user_id);
+      // Mont Views Params (ex: id=12313&id=124545...)
+      const userIdsParam = userIds.map((id) => `id=${id}`).join('&');
+
+      console.log("USERS", userIds);
+
+      // Máximo de usuários que podem ser buscados por vez
+      const batchSize = 100;
+      // Divida a lista de IDs de usuário em lotes menores
+      const batches = [] as any;
+      for (let i = 0; i < userIds.length; i += batchSize) {
+        batches.push(userIds.slice(i, i + batchSize));
+      }
+
+      // Faça várias chamadas à API do Twitch para cada lote de IDs de usuário
+      const usersData = [] as any;
+      for (const batch of batches) {
+        const batchIds = batch.join('&id=');
+        const { data: batchData } : any = await this.httpService.get(`https://api.twitch.tv/helix/users?${batchIds}`, {
+          headers
+        }).toPromise()
+        .catch((error) => {
+          Logger.error(error, 'Join');
+        });
+
+        usersData.push(...batchData.data);
+      }
+
+      const usersWithViewsAndImage = subs.map((follow) => {
+        const user = usersData.find((userData) => userData.id === follow.user_id);
+        return {
+          ...follow,
+          views: user?.view_count,
+          image: user?.profile_image_url,
+        };
+      });
+
+      const response = usersWithViewsAndImage.map((item, key) => ({
+        value: key,
+        color: "black",
+        selected: false,
+        currentMiddle: false,
+        img: item.image,
+        followed_at: item.followed_at,
+        from_id: item.user_login,
+        from_name: item.user_name,
+        is_gift: item.is_gift,
+      }));
+      
+      console.log("SUBS TOTAL", subs.length);
+      console.log("USERS TOTAL", usersData.length);
+
+       return { data: response };
   }
 
   async getViews(userId: string, accessToken: string): Promise<any> {
@@ -29,7 +105,7 @@ export class TwitchService {
       Authorization: `Bearer ${accessToken}`,
       'Client-ID': APP_CONFIG.TWITCH.clientID, // Substitua pelo ID da sua aplicação Twitch
     };
-    let subs: any[] = [];
+    let views: any[] = [];
     let cursor = '';
     do {
     const { data: followsData } : any = await this.httpService
@@ -55,15 +131,13 @@ export class TwitchService {
       throw new Error('A resposta da API do Twitch não está no formato esperado.');
     }
 
-    subs.push(...followsData.data); // save views
+    views.push(...followsData.data); // save views
     cursor = followsData.pagination.cursor; // save last page
 
     } while (cursor);
 
     // Get Views ID
-    const userIds: any = subs.map((follow) => follow.from_id);
-    // Mont Views Params (ex: id=12313&id=124545...)
-    const userIdsParam = userIds.map((id) => `id=${id}`).join('&');
+    const userIds: any = views.map((follow) => follow.from_id);
 
     // Máximo de usuários que podem ser buscados por vez
     const batchSize = 100;
@@ -87,7 +161,7 @@ export class TwitchService {
       usersData.push(...batchData.data);
     }
 
-    const usersWithViewsAndImage = subs.map((follow) => {
+    const usersWithViewsAndImage = views.map((follow) => {
       const user = usersData.find((userData) => userData.id === follow.from_id);
       return {
         ...follow,
@@ -108,7 +182,7 @@ export class TwitchService {
     }));
         
     // console.log(response);
-    console.log("VIEWS TOTAL", subs.length);
+    console.log("VIEWS TOTAL", views.length);
     console.log("USERS TOTAL", usersData.length);
     
     return { data: response };
